55a9fc7d0a5bd6ecd382917932aad7c00d32e080
fkadeal <infkadeal@gmail.com>
2023-03-25 07:11:22 +0300

    simple html js php fullstack no db

--- php/public/index.php
+++ php/public/index.php
@@ -5,3 +5,14 @@
  */
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
+
+
+$ch = curl_init("https://jsonplaceholder.typicode.com/comments?postId=3");
+
+curl_exec($ch);
+
+if (curl_error($ch)) {
+    curl_error($ch);
+}
+curl_close($ch);
+return  $ch;
--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-document.getElementById('search-input').addEventListener('keyup', async (e) => {
-    // Search comments
-    // Use this API: https://jsonplaceholder.typicode.com/comments?postId=3
-    // Display the results in the UI
+document.getElementById("search-input").addEventListener("keyup", async (e) => {
+  // Search comments
+  // Use this API: https://jsonplaceholder.typicode.com/comments?postId=3
+  // Display the results in the UI
 
-    // Things to look out for
-    // ---
-    // Use es6
+  // Things to look out for
+  // ---
+  // Use es6
+  var searchValue = document.getElementById("search-input").value;
+  let inner = document.getElementById("results");
+  inner.innerHTML = "";
+  const res = await fetch(`http://localhost:8000/${searchValue}`);
+  const json = await res.json();
 
-    const res = await fetch('http://localhost:8000/')
-    const json = await res.json()
+  console.log(json);
+  console.log("sdfs sdf ", searchValue);
 
-    const result = `<li>${json.join('</li><li>')}</li>`
-    document.getElementById('results').innerHTML = result
+  let result = json
+    .map(function (obj) {
+      if (
+        obj.name.toLowerCase().includes(searchValue) ||
+        obj.email.toLowerCase().includes(searchValue) ||
+        obj.body.toLowerCase().includes(searchValue)
+      ) {
+        return `<div class="searchresult"> 
+        <h2>${obj.name}</h2> 
+         <a href='mailto:${obj.email}'> ${obj.email}</a> 
+          <p> ${obj.body}</p> 
+          </div>
+        `;
+      }
+    })
+    .filter(Boolean);
 
-})
+  console.log("result ", result);
+  inner.innerHTML = result.join(" ");
+
+  console.log(result);
+});
